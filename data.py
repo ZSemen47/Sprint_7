e1904e04c3b5b7821c9681cd2b7849c66fde2e4d
Zhao Sen <zazulinsemen@gmail.com>
2026-04-21 23:54:42 +0300

    add data.py

---
+++ data.py
@@ -0,0 +1,9 @@
+BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/'
+COURIER_URL = 'courier'
+ORDER_URL = 'orders'
+COURIER_URL_FOR_DELETE = 'courier/'
+expected_result_create_409 = {'code': 409, 'message': 'Этот логин уже используется. Попробуйте другой.'}
+expected_result_ok_true = {'ok': True}
+expected_result_code_400 = {'code': 400, 'message': 'Недостаточно данных для создания учетной записи'}
+expected_result_login_not_found = {'code': 404, 'message': 'Учетная запись не найдена'}
+expected_result_login_bad_request = {'code': 400, 'message': 'Недостаточно данных для входа'}
